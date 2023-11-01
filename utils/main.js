3e8b5b8b1ff7e04f504b63ea08c54824a8a6606d
chen <1130443726@qq.com>
2023-11-01 16:11:46 +0800

    自动部署视频

---
+++ utils/main.js
@@ -0,0 +1,16 @@
+const fs = require('fs'); // 文件模块
+const files = fs.readdirSync("./")
+let filesName = []
+files.forEach((file) => {
+  const fileArr = file.split('.')
+  const suffix = fileArr[fileArr.length - 1]
+  if (suffix === "mp4") filesName.unshift(fileArr[0])
+})
+
+fs.writeFile('utils/filesName/names.json', JSON.stringify(filesName), function (err) {
+  if (err) {
+    console.log("写入失败" + err);
+  } else {
+    console.log("写入成功");
+  }
+})
