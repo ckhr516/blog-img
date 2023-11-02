b3231f7d22aab7f04ec7aeab6c8d40db9680e132
chen <1130443726@qq.com>
2023-11-02 11:36:11 +0800

    github URL

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -2,10 +2,11 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+githubUrl=https://ckhr516:${GITHUB_TOKEN}@github.com/ckhr516/ckhr516.github.io.git
 git config --global user.name "chen"
 git config --global user.email "1130443726@qq.com"
 
 git init
 git add utils/filesName/filesName.json
 git commit -m "HighLight video"
-git push -f https://ckhr516:$GITHUB_TOKEN@github.com/ckhr516/ckhr516.github.io.git main:test # 推送到github
+git push -f $githubUrl main:test # 推送到github
