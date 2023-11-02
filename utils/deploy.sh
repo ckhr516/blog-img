43bf33569eab51b971b722f48f82eae7f6bb18d2
chen <1130443726@qq.com>
2023-11-02 11:05:05 +0800

    asd

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -9,4 +9,4 @@ git config --global user.email "1130443726@qq.com"
 git init
 git add utils/filesName/filesName.json
 git commit -m "HighLight video"
-git push -f ${githubUrl} main:test # 推送到github
+git push -f https://ckhr516:${GITHUB_TOKEN}@github.com/ckhr516/ckhr516.github.io.git main:test # 推送到github
