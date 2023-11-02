1b0d95b2b6153667d65138c04ce3894a70411ea3
chen <1130443726@qq.com>
2023-11-02 15:01:18 +0800

    测试github actions

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -2,11 +2,11 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-githubUrl=https://ckhr516:${GITHUB_TOKEN}@github.com/ckhr516/ckhr516.github.io.git
+githubUrl=https://ckhr516:${GITHUB_TOKEN}@github.com/ckhr516/blog-img.git
 git config --global user.name "chen"
 git config --global user.email "1130443726@qq.com"
 
 git init
 git add utils/filesName/filesName.json
 git commit -m "HighLight video"
-git push -f git@github.com:ckhr516/ckhr516.github.io.git main:test # 推送到github
+git push -f ${githubUrl} main:test # 推送到github
